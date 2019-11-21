7a79cb8404648b602c36e3a59f3667552912ae79
Bruno Andrade <brunocea.1@gmail.com>
2019-11-21 16:27:12 +0000

    0.1.4

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -3,8 +3,8 @@ const { expect } = require('chai');
 
 describe('CLI', function() {
   it('should work', async function() {
-    this.timeout(20 * 10000);
+    this.timeout(20 * 100000000000000);
    //await cli(['-u','  https://www.accessibility.nl/wai-tools/validation-test-sites/cimot-melden/ ','-r', 'earl-a','-m','act','-act-rules','9eb3f6'])
-      await cli(['-u','  https://www.accessibility.nl/wai-tools/validation-test-sites/cimot-melden/ ','-r', 'earl-a','-m','act','-act-rules','9eb3f6'])
+      await cli(['-u','https://www.publico.pt/','-r', 'earl-a','-m','act'])
   });
 });
