34657e5e65bef3249409bf68938cbe017c32984a
João Vicente <joao.vicente@campus.ul.pt>
2020-05-05 16:13:52 +0100

    Updated README.md

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -2,15 +2,5 @@ const cli = require('../dist/index');
 const { expect } = require('chai');
 
 describe('CLI', function() {
-  it('should work', async function() {
-    this.timeout(10 * 10000);
-    
-    await cli(['-f', 'test/urls.txt', '-maxParallelEvaluations', '2', '-m', 'act', '-r', 'earl-a']);
-  });
-
-  it.only('should evaluate https://www.nav.no/', async function() {
-    this.timeout(1000 * 10000);
-    
-    await cli(['-u', 'https://www.nav.no/']);
-  });
+  //TODO: tests
 });
