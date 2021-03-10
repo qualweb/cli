7f48daac582ed8226d7be6cc832c4632c4841ff9
PedroSPSantos <pedrosimaops@gmail.com>
2021-03-10 17:56:19 +0000

    Changes to cli part 2

--- cli-master/test/cli.spec.js
+++
@@ -1,9 +0,0 @@
-import cli from '../dist/index';
-import { expect } from 'chai';
-import { exec } from 'child_process';
-
-describe('CLI', function() {
-  it('Print options', async function() {
-    cli();
-  });
-});
