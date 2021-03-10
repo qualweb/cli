e62eee77f8d79a52db0fec6e3bfcfdde9832a698
PedroSPSantos <pedrosimaops@gmail.com>
2021-03-10 19:13:11 +0000

    cli final x4

---
+++ bin/cli.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+'use strict';
+
+const cli = require('../dist/index');
+
+(async () => {
+  const args = Array.from(process.argv).slice(2);
+
+  try {
+    process.title = 'qw ' + args.join(' ');
+  } catch(err) {
+    process.title = 'qw';
+  }
+
+  await cli(args);
+})();
